329a86d1a147f17fa5b84f2e27c6d5ad3d7c617f
Nate Davis Olds <nate@Nates-MacBook-Pro.local>
2019-07-05 12:57:45 -0400

    Change alpha to development

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const AboutBlurb = () =>
     <h1 className="display-4">Anyone wanna play?</h1>
     <p className="lead">Play better matches more often.</p>
     <hr className="my-4" />
-    <p><strong>Currently in alpha.</strong> If you like playing pickleball and are in the Philadelphia, email us to become a beta tester.</p>
+    <p><strong>Currently in development.</strong> If you like playing pickleball and are in the Philadelphia, email us to become a beta tester.</p>
     <a className="btn btn-primary btn-lg" href="mailto:signup@anyonewannaplay.com" role="button">Become a beta tester</a>
   </div>
 
