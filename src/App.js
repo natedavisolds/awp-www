7075b8dcbf16fdcb121a9ea648c39dc2dfbf0897
Nate Davis Olds <nate@davisolds.com>
2020-07-13 22:18:38 -0400

    Change the link on the about to a button to prepare for netlify forms

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const AboutBlurb = () =>
     <p className="lead">Play better matches more often.</p>
     <hr className="my-4" />
     <p><strong>Currently in development.</strong> If you like playing pickleball and are in the Philadelphia, email us to become a beta tester.</p>
-    <a className="btn btn-primary btn-lg" href="mailto:signup@anyonewannaplay.com" role="button">Become a beta tester</a>
+    <button className="btn btn-primary btn-lg" role="button">Become a beta tester</button>
   </div>
 
 const App = () =>
