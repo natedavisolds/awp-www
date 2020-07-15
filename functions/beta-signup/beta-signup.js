8e6a591cb3d4672bbf5568e3ffe38668861edd3d
Nate Davis Olds <nate@davisolds.com>
2020-07-15 15:23:54 -0400

    Add email notification of beta signup

--- functions/beta-signup/beta-signup.js
+++ functions/beta-signup/beta-signup.js
@@ -1,7 +1,29 @@
 // const fetch = require('node-fetch')
 var Airtable = require('airtable');
+const sgMail = require('@sendgrid/mail');
 
 const apiKey = process.env.AIRTABLE_API_KEY
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendConfirmationEmailTo = user => {
+  return new Promise(resolve => {
+    const msg = {
+      to: user.email,
+      from: 'nate@davisolds.com',
+      subject: 'Welcome to Anyone Wanna Play BETA',
+      text: `Thank you ${user.first_name} for your interest in being a beta tester.  When we have something ready to begin testing we will contact you.`,
+      html: `<strong>Thank you ${user.first_name} for your interest in being a beta tester.</strong>  When we have something ready to begin testing we will contact you.`,
+    };
+    try {
+      sgMail.send(msg)
+      console.info(`Sent to ${user.email}`)
+      resolve(`Sent to ${user.email}`)
+    } catch(err) {
+      console.error(err)
+      resolve(`Error when sending to ${user.email}`)
+    }
+  })
+}
 
 function createUser(first_name,last_name,email) {
   var airtableBase = new Airtable({apiKey: apiKey}).base('appToEv1MITYg6kqf');
@@ -25,13 +47,13 @@ function createUser(first_name,last_name,email) {
           resolve({})
           return;
         }
-        console.info("Created.");
 
         var users = []
 
         records.forEach(function (record) {
-          console.log(record.getId());
-          users.push({id:record.getId()})
+          var user = Object.assign({},record.fields,{id:record.getId()})
+          sendConfirmationEmailTo(user)
+          users.push(user)
         });
 
         resolve({users})
